3c7c7103251eb08bff81dee4b07e71965b0ee869
FernandoGrangeiro <fernandojgrangeiro@gmail.com>
2020-10-07 19:59:22 -0300

    Teste

--- app/src/androidTest/java/com/fgrangeiro/cipresentation/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/fgrangeiro/cipresentation/ExampleInstrumentedTest.kt
@@ -1,12 +1,11 @@
 package com.fgrangeiro.cipresentation
 
-import androidx.test.platform.app.InstrumentationRegistry
+import android.util.Log
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
